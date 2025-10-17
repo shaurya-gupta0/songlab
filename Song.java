452b337e87e52a0e481e26d8cf50ea4d76aae4f1
shaurya-gupta0 <sguptafamily4@gmail.com>
2025-10-17 09:25:16 -0700

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,16 +1,20 @@
 public class Song {
     // artist_name;track_name;release_date;genre;len;shake the audience;obscene;danceability;loudness;topic
     private String artist, track, genre, topic;
-    private int year, releaseDate, length,loudness;
-    private double danceability;
-    private boolean obscene;
+    private int year, releaseDate, length;
+    private double danceability, shake, obscene, loudness;
 
-    public Song(String artist, String title, String genre, int year, double danceability) {
+    public Song(String artist, String track, int release_date, String genre, int duration, double shake, double obscene, double danceability, double loudness, String topic) {
         this.artist = artist;
-        this.title = title;
+        this.track = track;
         this.genre = genre;
         this.year = year;
         this.danceability = danceability;
+        this.topic = topic;
+        this.length = length;
+        this.shake = shake;
+        this.obscene = obscene;
+        this.loudness = loudness;
     }
 
 }
